92f61b65944ef35bea58de6aed90017c89bf8ca7
Ramles75 <elmarseyidov76@gmail.com>
2024-03-22 19:31:12 +0400

    files add

---
+++ main.js
@@ -0,0 +1,28 @@
+const textInput = document.getElementById('text-input');
+const askBtn = document.getElementById('btn');
+const taskList = document.getElementById('task-list');
+
+const ToDoAdd = () => {
+    const todoText = textInput.value.trim();
+    if (todoText !== "") {
+        const listItem = document.createElement("li");
+        listItem.textContent = todoText;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = '<i class="far fa-trash-alt"></i>';
+        listItem.appendChild(deleteButton);
+
+        deleteButton.addEventListener("click", () => {
+            listItem.remove();
+        });
+
+        taskList.appendChild(listItem);
+
+        textInput.value = "";
+    } else {
+        alert("Please add task");
+    }
+};
+
+askBtn.addEventListener("click", ToDoAdd);
+
